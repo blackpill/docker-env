649eeda743bad3b597cb5bdc87c62169a14f5871
blackpill <10999866@qq.com>
2016-07-15 19:07:59 +0800

    add NFS_DIR

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,6 +4,6 @@
 
 mkdir -p $WWWROOT
 chown -R www-data.www-data $WWWROOT
-mount -t nfs $NFS_SERVER:/wwwroot $WWWROOT
+mount -t nfs $NFS_SERVER:$NFS_DIR $WWWROOT
 /render-templates.sh /etc/nginx/sites-templates /etc/nginx/conf.d
 exec $@
