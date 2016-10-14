d14cae5c2b42fd57a266dadd0652a39769616768
blackpill <10999866@qq.com>
2016-10-14 14:08:33 +0800

    add USE_NFS

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -4,5 +4,10 @@
 
 mkdir -p $WWWROOT
 chown -R www-data.www-data $WWWROOT
-mount -t nfs $NFS_SERVER:$NFS_DIR $WWWROOT
+if [ "$USE_NFS" = "1" ]; then	
+    mount -t nfs $NFS_SERVER:$NFS_DIR $WWWROOT
+    echo Use NFS
+else
+    echo Use VFS
+fi
 php-fpm
