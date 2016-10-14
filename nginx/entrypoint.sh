9ca39d93d6bf0f0811dc7542e9d635f44ce72c18
blackpill <10999866@qq.com>
2016-10-14 12:26:34 +0800

    add USE_NFS

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,6 +4,12 @@
 
 mkdir -p $WWWROOT
 chown -R www-data.www-data $WWWROOT
-mount -t nfs $NFS_SERVER:$NFS_DIR $WWWROOT
+if [ "$USE_NFS" = "1" ]; then	
+    mount -t nfs $NFS_SERVER:$NFS_DIR $WWWROOT
+    echo Use NFS
+else
+    echo Use VFS
+fi
+
 /render-templates.sh /etc/nginx/sites-templates /etc/nginx/conf.d
 exec $@
