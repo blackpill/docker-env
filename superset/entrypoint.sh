2ec56dae75d87b3696ef0ca509804e89567e7131
blackpill <10999866@qq.com>
2017-10-12 15:05:58 +0800

    add npm for debug

---
+++ superset/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#set -eu
+
+mkdir -p $WWWROOT
+chown -R www-data.www-data $WWWROOT
+if [ "$USE_NFS" = "1" ]; then	
+    mount -t nfs $NFS_SERVER:$NFS_DIR $WWWROOT
+    echo Use NFS
+else
+    echo Use VFS
+fi
+php-fpm
